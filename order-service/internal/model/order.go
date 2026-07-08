7c79aa8df94da1ea0e287dbda5e89a4c11e6f061
Tima <temirlanaltaev26092004@gmail.com>
2026-07-08 21:07:44 +0500

    order-service: updated model

--- order-service/internal/model/order.go
+++ order-service/internal/model/order.go
@@ -2,18 +2,43 @@ package model
 
 import "time"
 
+type OrderStatus string
+
+const (
+	OrderCreated    OrderStatus = "PENDING"
+	OrderUpdated    OrderStatus = "CONFIRMED"
+	OrderProcessing OrderStatus = "PROCESSING"
+	OrderDelivered  OrderStatus = "DELIVERED"
+	OrderCancelled  OrderStatus = "CANCELLED"
+)
+
 type Orders struct {
-	ID          int          `json:"id"`
-	UserID      int          `json:"user_id"`
-	Status      string       `json:"status"`
-	TotalAmount float64      `json:"totalAmount"`
-	CreatedAt   time.Time    `json:"created_at"`
-	Items       []OrderItems `json:"items"`
+	Version          int                     `json:"version"`
+	OrderID          int                     `json:"order_id"`
+	UserID           int                     `json:"user_id"`
+	Status           OrderStatus             `json:"status"`
+	TotalAmountCents int64                   `json:"total_amount_cents"`
+	CreatedAt        time.Time               `json:"created_at"`
+	Items            []OrderItem `json:"items"`
 }
-type OrderItems struct {
-	ID        int     `json:"id"`
-	OrderId   int     `json:"order_id"`
-	ProductID int     `json:"product_id"`
-	Quantity  int     `json:"quantity"`
-	Price     float64 `json:"price"`
+type OrderItem struct {
+	ID              int
+	OrderID         int
+	ProductID       int
+	Quantity        int
+	UnitPriceCents int64
+}
+type OrderCreatedEvent struct {
+	Version int         `json:"version"`
+	OrderID int         `json:"order_id"`
+	UserID  int         `json:"user_id"`
+	Status  OrderStatus `json:"status"`
+	TotalAmountCents int64 `json:"total_amount_cents"`
+	CreatedAt time.Time `json:"created_at"`
+	Items []OrderCreatedEventItem `json:"items"`
+}
+type OrderCreatedEventItem struct {
+	ProductID  int   `json:"product_id"`
+	Quantity   int   `json:"quantity"`
+	PriceCents int64 `json:"price_cents"`
 }
