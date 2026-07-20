199d45deb7767e70cc5f4fd083d8ea4b1a063276
Tima <temirlanaltaev26092004@gmail.com>
2026-07-20 20:02:00 +0500

    add GetQuotes to router

--- inventory-service/cmd/main.go
+++ inventory-service/cmd/main.go
@@ -49,7 +49,7 @@ func main() {
 		r.Post("/products", handler.InsertProduct)
 		r.Put("/products/{id}/quantity", handler.UpdateQuantity)
 		r.Put("/products/{id}/price", handler.UpdatePrice)
-		r.Post("/products/quotes", handler.w)
+		r.Post("/products/quotes", handler.GetQuotes)
 	})
 	r.Get("/products", handler.GetAllProducts)
 	err := http.ListenAndServe(":8084", r)
